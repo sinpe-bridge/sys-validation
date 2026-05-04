e75894a119a867bab589b7e2167a76e050c0456a
dennis-gomez <dennis.gomez1310@gmail.com>
2026-05-03 22:00:03 -0600

    Message model
    Creation of message model to validate the payment which is recived by the cell phone.

---
+++ src/models/message.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Message = sequelize.define('Message', {
+    message_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    message_amount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }, 
+    message_details: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    message_user_name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    message_reference_code: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    message_date_time: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }, 
+    message_create_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    }
+}, {
+    tableName: 'messages',
+    timestamps: false,
+}); 
+
+module.exports = Message;
